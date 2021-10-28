2de2a501b59d8a981618cea889020d8cc61785c3
Mandeep Mehra <mandeep.mehra@xebia.com>
2021-10-28 13:14:33 -0700

    ec2 module

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 module "ec2" {
-  source        = "./module"
+  # source        = "./module"
+  source  = "mandeepmehra/ec2module/aws"
+  # source = "git::https://bitbucket.epsilon.com?ref=0.0.1"
+  version = "0.0.1" 
   instanceCount = 1
   instanceType  = "t2.micro"
   amiId         = "ami-09e67e426f25ce0d7"
   envType       = "DEV"
 }
+
 
 output "ipaddress" {
   value = module.ec2.ipaddress
--- provider.tf
+++ provider.tf
@@ -3,5 +3,9 @@ provider "aws" {
 }
 
 terraform {
-
+  backend "s3" {
+    bucket = "mandeeptrainingoctobertfstate"
+    key = "tfstate"
+    region = "ap-south-1"
+  }
 }
